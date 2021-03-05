4df5c732ea5eacfcfb40909219f78843c36f1340
Joseph Budina <joebud3@gmail.com>
2021-03-05 09:54:07 -0700

    Add models and controllers add index method and index view

---
+++ app/models/flight.rb
@@ -0,0 +1,8 @@
+class Flight < ApplicationRecord
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
+
+  def self.order_by_departure
+    order(:departure_city)
+  end
+end
